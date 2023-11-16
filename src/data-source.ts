793fbf5ac81b43e6e584814ded008f2f5da94d15
Roubilibo <roubilridlo@gmail.com>
2023-11-16 12:48:14 +0700

    deploy database

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,14 +3,15 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
 	type: "postgres",
-	host: "localhost",
+	host: "ep-bold-grass-13341484.ap-southeast-1.aws.neon.tech",
 	port: 5432,
-	username: "postgres",
-	password: "root",
+	username: "roubilibo",
+	password: "NbYgS63ToCBh",
 	database: "db_quiz_whatw",
 	synchronize: true,
 	logging: false,
 	entities: ["src/entity/*.ts"],
 	migrations: ["src/migration/*.ts"],
 	subscribers: [],
+	ssl: true,
 });
