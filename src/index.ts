ab4e339c0cd0c8730e432121bad1bc1e181a5bfb
afadz <arya.skoba@gmail.com>
2023-12-07 13:47:34 +0700

    midtrans

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,83 @@
 import { AppDataSource } from "./data-source";
-import * as express from "express";
-import * as cors from "cors";
-import * as http from "http";
 import { Server } from "socket.io";
 import SocketServices from "./services/SocketServices";
 import AvaRouter from "./routes/Avatars";
 import UserRouter from "./routes/Users";
+import express = require("express");
+import http = require("http");
+import cors = require("cors");
+import midtransClient = require("midtrans-client");
 
 AppDataSource.initialize()
-	.then(async () => {
-		const app = express();
-		const server = http.createServer(app);
-		const io = new Server(server);
-		// Initialize the SocketService with the io instance
-		const socket = new SocketServices(io);
+  .then(async () => {
+    const app = express();
+    const server = http.createServer(app);
+    const io = new Server(server);
+    const socket = new SocketServices(io);
 
-		//Port
-		const port = 3000;
+    const snap = new midtransClient.Snap({
+      isProduction: false,
+      serverKey: "SB-Mid-server-3mi0pq2O0MesF3jHIStaVVTo",
+      clientKey: "SB-Mid-client-2yxSDW9MSfHHPxRd",
+    });
 
-		const options: cors.CorsOptions = {
-			allowedHeaders: [, "X-Requested-With", "Content-Type", "Authorization"],
-			credentials: true,
-			methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-			origin: "*",
-			preflightContinue: false,
-		};
+    const port = 3000;
 
-		app.use(express.json());
-		app.use(cors(options));
+    const options = {
+      allowedHeaders: ["X-Requested-With", "Content-Type", "Authorization"],
+      credentials: true,
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      origin: "*",
+      preflightContinue: false,
+    };
 
-		//! Routes
-		app.use("/api", AvaRouter);
-		app.use("/api", UserRouter);
+    app.use(express.json());
+    app.use(cors(options));
 
-		server.listen(port, () => {
-			console.log(`Server is running on port ${port}`);
-		});
-	})
-	.catch((error) => console.log(error));
+    app.use("/api", AvaRouter);
+    app.use("/api", UserRouter);
+
+    // New route for Midtrans transaction
+    app.post("/api/midtrans/transaction", async (req, res) => {
+      const { id, price, quantity, email, fullname } = req.body;
+
+      const parameter = {
+        transaction_details: {
+          order_id: `ORDER-${Date.now()}-${Math.floor(Math.random() * 1000)}`,
+          gross_amount: price,
+          quantity: quantity,
+        },
+        customer_details: {
+          fullname: fullname,
+          email: email,
+        },
+      };
+
+      try {
+        const payment = await snap.createTransaction(parameter);
+        const payment_url = payment.redirect_url;
+        const token = payment.token;
+        const orderId = parameter.transaction_details.order_id; 
+        console.log(payment);
+
+        return res.status(200).json({
+          code: 200,
+          message: "Success",
+          data: {
+            message: "Transaction success",
+            payment_url: payment_url,
+            token: token,
+            orderId: orderId,
+          },
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+      }
+    });
+
+    server.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => console.log(error));
--- src/services/AvatarServices.ts
+++ src/services/AvatarServices.ts
@@ -13,85 +13,85 @@ export default new (class AvatarServices {
 	private readonly UserRepository: Repository<User> =
 		AppDataSource.getRepository(User);
 
-	async find(req: Request, res: Response): Promise<Response> {
-		try {
-			const avatar = await this.AvaRepository.find();
+  async find(req: Request, res: Response): Promise<Response> {
+    try {
+      const avatar = await this.AvaRepository.find();
 
-			return res.status(200).json(avatar);
-		} catch (error) {
-			return res.status(500).json(error);
-		}
-	}
+      return res.status(200).json(avatar);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
 
-	async findById(req: Request, res: Response): Promise<Response> {
-		try {
-			const avatar = await this.AvaRepository.findOneBy({
-				id: parseInt(req.params.id),
-			});
-			return res.status(200).json(avatar);
-		} catch (error) {
-			return res.status(500).json(error);
-		}
-	}
-	async create(req: Request, res: Response): Promise<Response> {
-		try {
-			const data = req.body;
+  async findById(req: Request, res: Response): Promise<Response> {
+    try {
+      const avatar = await this.AvaRepository.findOneBy({
+        id: parseInt(req.params.id),
+      });
+      return res.status(200).json(avatar);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+  async create(req: Request, res: Response): Promise<Response> {
+    try {
+      const data = req.body;
 
-			const { error, value } = createAvatarSchema.validate(data);
-			if (error) {
-				return res.status(400).json({ error: error.details[0].message });
-			}
-			let image = "";
-			if (req.file?.filename) {
-				image = await uploadToCloudinary(req.file);
+      const { error, value } = createAvatarSchema.validate(data);
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+      let image = "";
+      if (req.file?.filename) {
+        image = await uploadToCloudinary(req.file);
 
-				deleteFile(req.file.path);
-			}
-			console.log(value);
+        deleteFile(req.file.path);
+      }
+      console.log(value);
 
-			const avatar = this.AvaRepository.create({
-				image: image,
-				price: value.price,
-			});
-			const createAva = await this.AvaRepository.save(avatar);
-			return res.status(200).json(createAva);
-		} catch (error) {
-			return res.status(500).json(error);
-		}
-	}
+      const avatar = this.AvaRepository.create({
+        image: image,
+        price: value.price,
+      });
+      const createAva = await this.AvaRepository.save(avatar);
+      return res.status(200).json(createAva);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
 
-	async delete(req: Request, res: Response): Promise<Response> {
-		try {
-			const avatar = await this.AvaRepository.delete({
-				id: parseInt(req.params.id),
-			});
-			return res.status(200).json(avatar);
-		} catch (error) {
-			return res.status(500).json(error);
-		}
-	}
+  async delete(req: Request, res: Response): Promise<Response> {
+    try {
+      const avatar = await this.AvaRepository.delete({
+        id: parseInt(req.params.id),
+      });
+      return res.status(200).json(avatar);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
 
-	async userFindAvatars(req: Request, res: Response): Promise<Response> {
-		try {
-			const avatars: Avatar[] = await this.AvaRepository.find({
-				order: {
-					id: "ASC",
-				},
-				relations: ["avatar_owners"],
-				select: {
-					avatar_owners: {
-						id: true,
-						fullname: true,
-						email: true,
-					},
-				},
-			});
+  async userFindAvatars(req: Request, res: Response): Promise<Response> {
+    try {
+      const avatars: Avatar[] = await this.AvaRepository.find({
+        order: {
+          id: "ASC",
+        },
+        relations: ["avatar_owners"],
+        select: {
+          avatar_owners: {
+            id: true,
+            fullname: true,
+            email: true,
+          },
+        },
+      });
 
-			return res.status(200).json({
-				code: 200,
-				status: "succes",
-				data: avatars.map((avatar) => {
-					let owned = false;
+      return res.status(200).json({
+        code: 200,
+        status: "succes",
+        data: avatars.map((avatar) => {
+          let owned = false;
 
 					if (avatar.price === 0) {
 						owned = true;
